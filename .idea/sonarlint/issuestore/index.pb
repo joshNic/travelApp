646ce377551f6e943c8af005b5aa654e8d05d002
Joshua Mugisha <joshuamugisha@MacBook-Pro-90.local>
2019-08-04 23:52:35 +0300

    first commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,19 @@
+
+<
+build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
+P
+ app/src/main/AndroidManifest.xml,8/c/8c55c3ccc257e5907959013f99656e4c8ec3903e
+k
+;app/src/main/java/com/example/travelmetrics/TravelDeal.java,1/4/14d82231e4fe2b491aa1d9f984ff4f042eca3324
+m
+=app/src/main/java/com/example/travelmetrics/MainActivity.java,5/a/5a42263a690a7a4474d64fe9eec717cefc6cc2f7
+m
+=app/src/main/java/com/example/travelmetrics/FirebaseUtil.java,b/b/bbb57cfda4be498a1809914c5006ee15e14ea851
+l
+<app/src/main/java/com/example/travelmetrics/DealAdapter.java,e/c/ec16892d611e8440e7ef5bf7b5a9bdbb7d8f7f92
+m
+=app/src/main/java/com/example/travelmetrics/ListActivity.java,0/b/0bf5b674b1b0fbc4d5f72aec4563a394fb6d18a5
+@
+app/build.gradle,f/4/f4a01d6a4fcb971362ec00a83903fd3902f52164
+H
+app/google-services.json,f/d/fd8d34b142d02ec743a8efefefd2c097961666e9
---
+++ app/src/main/java/com/example/travelmetrics/ListActivity.java
@@ -0,0 +1,80 @@
+package com.example.travelmetrics;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.list_activity_menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.insert_menu);
+        if (FirebaseUtil.isAdmin ==true){
+            menuItem.setVisible(true);
+        }else{
+            menuItem.setVisible(false);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.insert_menu:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.logout_menu:
+                AuthUI.getInstance()
+                        .signOut(this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            public void onComplete(@NonNull Task<Void> task) {
+                                FirebaseUtil.attachListener();
+                            }
+                        });
+                FirebaseUtil.detachListener();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        FirebaseUtil.detachListener();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        FirebaseUtil.openFbReference("traveldeals", this);
+        RecyclerView rvDeals = findViewById(R.id.rvDeals);
+        final DealAdapter adapter = new DealAdapter();
+        rvDeals.setAdapter(adapter);
+        LinearLayoutManager dealsLayoutManager = new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
+        rvDeals.setLayoutManager(dealsLayoutManager);
+        FirebaseUtil.attachListener();
+    }
+    public void showMenu(){
+        invalidateOptionsMenu();
+    }
+}
